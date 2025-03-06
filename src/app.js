eae3b5300d99d538e3445d231ffbacc2ab7a13e3
Julian David Ortega Solarte <95juliandos@gmail.com>
2025-03-06 00:44:28 +0000

    ya listo

--- src/app.js
+++ src/app.js
@@ -1,11 +1,36 @@
-import "bootstrap";
-import "./style.css";
+const pronoun = ['the', 'our'];
+const adj = ['great', 'big'];
+const noun = ['jogger', 'racoon'];
+const extensions = ['.com', '.net', '.us', '.io'];
 
+function generateDomains() {
+    let domains = [];
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+    for (let p of pronoun) {
+        for (let a of adj) {
+            for (let n of noun) {
+                for (let ext of extensions) {
+                    let domain = p + a + n + ext;
+                    domains.push(domain);
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+                    if (n.endsWith(ext.replace(".", ""))) {
+                        let hackedDomain = p + a + "." + n;
+                        domains.push(hackedDomain);
+                    }
+                }
+            }
+        }
+    }
+    return domains;
+}
+
+document.getElementById("generateBtn").addEventListener("click", () => {
+    const domainList = document.getElementById("domainList");
+    domainList.innerHTML = "";
+    
+    generateDomains().forEach(domain => {
+        const li = document.createElement("li");
+        li.textContent = domain;
+        domainList.appendChild(li);
+    });
+});
